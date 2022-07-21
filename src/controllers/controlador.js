f2a4839b964932f619ae318621cd6606e249dbef
malvasar <marina.marin75.mm@gmail.com>
2022-07-21 17:44:33 -0500

    Creando Tienda

---
+++ src/controllers/controlador.js
@@ -0,0 +1,12 @@
+//CREANDO VARIABLES
+let nombreUsuario="Luz Marina Marin"
+const EDAD_USUARIO=47
+let estaturaUsuario=1.50
+let hinchaDelVerde=false
+
+console.log(nombreUsuario)
+EDAD_USUARIO
+console.log(EDAD_USUARIO)
+
+
+
